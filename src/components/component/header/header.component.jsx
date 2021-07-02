26d441d24fd24b3f91e48cff568eaea25a70201b
satish <satish3r3@gmail.com>
2021-07-02 15:55:03 -0400

    commit

--- src/components/component/header/header.component.jsx
+++ src/components/component/header/header.component.jsx
@@ -8,7 +8,7 @@ const Header = () => (
       </Link>
       <div className = "options">
       <Link to = "/shop" className = "option">
-         SHOP</Link>
+         SHOPIT</Link>
       <Link to = "/contact" className ="option">
             CONTACT</Link>
       </div>
